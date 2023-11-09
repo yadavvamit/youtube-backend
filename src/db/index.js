657b447399e7d160d1e8b78683a99888866aebd3
yadavvamit <yadavvamit223222@gmail.com>
2023-11-09 16:14:46 +0530

    connect to mongoDB

--- src/db/index.js
+++ src/db/index.js
@@ -9,7 +9,7 @@ const connectDB = async () =>{
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         
     } catch (error) {
-        console.log("MONGODB connection failed:" , error);
+        console.log("MONGODB connection error:" , error);
         process.exit(1);
         
     }
