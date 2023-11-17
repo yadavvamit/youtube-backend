7e5a2aab95562f8976227706d332948f7c90fc5f
yadavvamit <yadavvamit223222@gmail.com>
2023-11-17 15:13:53 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { error, log } from "console";
-import connectDB  from "./db/index.js";
 
+import connectDB  from "./db/index.js";
 import dotenv from "dotenv"
+import express from "express"
+const app =  express()
 
 dotenv.config({
     path: './env'
-});
+})
 
 connectDB()
 
